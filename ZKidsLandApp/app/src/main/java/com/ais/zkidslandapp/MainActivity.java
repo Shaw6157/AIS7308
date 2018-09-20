464668b2b5aa86b59a08b0f9f489a2aaf6bd6c48
Shaw6157 <43285754+Shaw6157@users.noreply.github.com>
2018-09-21 00:08:34 +1200

    part maths and calcu
    add codes of maths in class
    and a little modification at home

--- ZKidsLandApp/app/src/main/java/com/ais/zkidslandapp/MainActivity.java
+++ ZKidsLandApp/app/src/main/java/com/ais/zkidslandapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +15,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     public void btnToQ (View v){
         Intent intentToQ = new Intent(this, QuestionActivity.class);
+
+        switch (v.getId()) {
+            case R.id.btnPlus:
+                intentToQ.putExtra("p_operator", "+");
+                break;
+            case R.id.btnMinus:
+                intentToQ.putExtra("p_operator", "-");
+                break;
+            case R.id.btnMultiply:
+                intentToQ.putExtra("p_operator", "*");
+                break;
+            case R.id.btnDivide:
+                intentToQ.putExtra("p_operator", "/");
+                break;
+            default:
+                intentToQ.putExtra("p_operator", "");
+                break;
+        }
+
         startActivity(intentToQ);
     }
 
--- ZKidsLandApp/app/src/main/java/com/ais/zkidslandapp/QuestionActivity.java
+++ ZKidsLandApp/app/src/main/java/com/ais/zkidslandapp/QuestionActivity.java
@@ -2,12 +2,91 @@ package com.ais.zkidslandapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class QuestionActivity extends AppCompatActivity {
+
+    TextView tvTitle_Q, tvQuestion;
+    Button btnNum1, btnNum2, btnNum3, btnNum4;
+
+    int gv_answer = 0;
+    int gv_numbtn = 4;
+    String pi_operator = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        tvTitle_Q = (TextView) findViewById(R.id.tvTitle_Q);
+        tvQuestion = (TextView) findViewById(R.id.tvQuestion);
+
+        btnNum1 = (Button) findViewById(R.id.btnNum1);
+        btnNum2 = (Button) findViewById(R.id.btnNum2);
+        btnNum3 = (Button) findViewById(R.id.btnNum3);
+        btnNum4 = (Button) findViewById(R.id.btnNum4);
+
+        pi_operator = this.getIntent().getExtras().get("p_operator").toString();
+
+        loadData();
     }
+
+    public void btnCheck (View v){
+        loadData();
+    }
+
+    private void loadData(){
+        int lv_num1 = rollDice(10);
+        int lv_num2 = rollDice(10);
+
+//        String lv_operator = "+";
+
+        tvQuestion.setText(lv_num1 + pi_operator + lv_num2 + " = ? ");
+
+        gv_answer = findAnswer(pi_operator, lv_num1, lv_num2);
+        placeNumbers();
+
+    }
+
+    private int findAnswer(String operator, int num1, int num2){
+        if (operator.equals("+")){
+            return num1 + num2;
+        } else if (operator.equals("-")){
+            return num1 - num2;
+        } else if (operator.equals("*")){
+            return num1 * num2;
+        } else {
+            return num1 / num2;
+        }
+    }
+
+    private void placeNumbers(){
+        int lv_postion = rollDice(gv_numbtn);
+
+        Button btns[] = {btnNum1, btnNum2, btnNum3, btnNum4};   //hard code
+        for (int i = 0; i < gv_numbtn; i++){
+            if (lv_postion == i){
+                btns[i].setText("" + gv_answer);
+            } else {
+                btns[i].setText("" + generateWrongAnswer());
+            }
+        }
+    }
+
+    private int generateWrongAnswer(){
+        return new Random().nextInt(8) + gv_answer - 3;
+    }
+
+    private int rollDice(int sides) {
+        return new Random().nextInt(sides);
+    }
+
+
+
+
+
 }
